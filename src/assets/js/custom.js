a7248d067c4d6d503d9019f9b83051d852887ced
Derick Moncado <derickmoncado@gmail.com>
2021-07-02 16:23:30 -0700

    Clear out

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,23 +1,14 @@
-'use strict';
+"use strict";
 
-let pathname = window.location.pathname; // Gets URL path and stores in pathname
+const pathname = window.location.pathname; // Gets URL path and stores in pathname
 
-$(document).ready(function() {
-	console.log('document ready!');
-	//removeIf(production)
-		// Be careful putting code here!
-	//endRemoveIf(production)
-
-	// For appending the 'active' class
-	$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
-	$('.dropdown-menu > li > a[href="'+pathname+'"]').addClass('active');
-
-	// Initiate Slick
-	$('.home-slick-carousel').slick({
-		dots: true,
-		mobileFirst: true,
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1
-	});
+$(document).ready(function () {
+  // Initiate Slick
+  $(".cod-legacy-carousel").slick({
+    dots: true,
+    mobileFirst: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
 });
